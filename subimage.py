c17356fa6d4aa03abbfde0f8d1b2e02f55568413
Rabih Kodeih <rabih.kodeih@gmail.com>
2019-01-02 12:30:46 +0200

    Convert command line output to json format

--- subimage.py
+++ subimage.py
@@ -1,33 +1,35 @@
 import sys
+import json
 from settings import CONFIDENCE_THRESHOLD
 from common.utils import cropped
 from common.utils import UnsupportedFormatOrMissingFile
 
 
 if __name__ == '__main__':
-    sys.stdout.write('\n')
 
     image_1 = './unit_tests/images/test1.jpg'
-    image_2 = './unit_tests/images/test4.jpg'
+    image_2 = './unit_tests/images/test2.jpg'
     confidence_threshold = CONFIDENCE_THRESHOLD
     # TODO: args, image1 and image2 and cutoff_confidence (optional, defaults to the value in settings)
+    # TODO: add help text explaining the output format and interpretation
 
     try:
         match, index, confidence = cropped(image_1, image_2, confidence_threshold)
     except UnsupportedFormatOrMissingFile:
-        sys.stdout.write('Error: One or both of input images is missing or has an unsupported format.\n')
+        result = {'status': 'ERROR',
+                  'message': 'One or both of input images is either missing or has an unsupported format.'}
     else:
         if match is None:
-            sys.stdout.write('No image appears to be cropped.\n')
+            result = {'status': 'OK',
+                      'cropped': None,
+                      'top-left': None,
+                      'confidence': confidence}
         else:
-            cropped_image = [image_1, image_2][index]
-            sys.stdout.write('Image "{}" appears to be cropped.\n'.format(cropped_image))
-            sys.stdout.write('Top left corner (x-horizontal, y-vertical) : {}\n'.format(match[0]))
-        if confidence > 0.0:
-            sys.stdout.write('Confidence score : {}\n'.format(confidence))
-        else:
-            sys.stdout.write('Incompatible dimenssions\n')
-
+            result = {'status': 'OK',
+                      'cropped': [image_1, image_2][index],
+                      'top-left': {'x': match[0][0], 'y': match[0][1]},
+                      'confidence': confidence}
+    json.dump(result, sys.stdout, indent=2)
     sys.stdout.write('\n')
 
 
